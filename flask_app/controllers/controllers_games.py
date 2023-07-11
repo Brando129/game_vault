d81087010ca85e4b3b29816ad69bfc5b379a40c5
Brandon Colangelo <brandoncancode@gmail.com>
2023-07-11 16:50:15 -0400

    Built the back to your collection route.

--- flask_app/controllers/controllers_games.py
+++ flask_app/controllers/controllers_games.py
@@ -15,7 +15,7 @@ def new_game():
     print("Add new game route successful...")
     return render_template('add_game.html', user=models_user.User.get_user_by_id(data))
 
-# Route to take the user back to their collection.
+# Route to take the user back to the home page.
 @app.route('/your_collection')
 def back_to_collection():
     print("Back to the collection route...")
@@ -26,3 +26,15 @@ def back_to_collection():
     }
     print("Back to the collection route successful...")
     return render_template('homepage.html', user=models_user.User.get_user_by_id(data))
+
+# Route to take the user to the edit game page.
+@app.route('/edit_game')
+def edit_game():
+    print("Edit game route...")
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data = {
+        "id": session['user_id']
+    }
+    print("Edit game route successful...")
+    return render_template('edit_game.html', user=models_user.User.get_user_by_id(data))
