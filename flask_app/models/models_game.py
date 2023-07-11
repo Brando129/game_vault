4cc7e2acc69f6e4cbca23da6c4d8903dd97f4e88
Brandon Colangelo <brandoncancode@gmail.com>
2023-07-11 09:57:19 -0400

    Built get_all_games and get_by_title classmethods.

--- flask_app/models/models_game.py
+++ flask_app/models/models_game.py
@@ -25,3 +25,25 @@ class Game:
                 VALUES (%(title)s, %(release_date)s, %(genere)s, %(console)s, %(description)s);"""
         print("Saving game method was successful...")
         return connectToMySQL(db).query_db(query, data)
+
+    # Classmethod for getting all the games.
+    @classmethod
+    def get_all_games(cls):
+        print("Getting all the games method...")
+        query = "SELECT * FROM games;"
+        results = connectToMySQL(db).query_db(query)
+        games = []
+        for row in results:
+            games.append(cls(row))
+        print("Getting all the games method was successful...")
+        return games
+
+    # Classmethod for getting a game by its title.
+    @classmethod
+    def get_by_title(cls, data):
+        print("Getting the game by title method...")
+        query = "SELECT * FROM games WHERE title = %(title)s;"
+        results = connectToMySQL(db).query_db(query, data)
+        print("Getting the game by title method was successful...")
+        return cls(results[0])
+
