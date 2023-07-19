5e595c53ae6e1886db3f3c6f1e69dee341c6d21a
Brandon Colangelo <brandoncancode@gmail.com>
2023-07-19 17:32:56 -0400

    Built the routes for rendering all the HTML pages.

--- flask_app/controllers/controllers_games.py
+++ flask_app/controllers/controllers_games.py
@@ -9,3 +9,31 @@ def render_consoles_page():
     if 'user_id' not in session:
         return redirect('/logout')
     return render_template('consoles.html')
+
+# Route for rendering the "games" page.
+@app.route('/games')
+def render_games_page():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    return render_template('games.html')
+
+# Route for rendering the "peripherals" page.
+@app.route('/peripherals')
+def render_peripherals_page():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    return render_template('peripherals.html')
+
+# Route for rendering the "print media" page.
+@app.route('/print_media')
+def render_print_media_page():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    return render_template('print_media.html')
+
+# Route for rendering the "collection summary" page.
+@app.route('/collection_summary')
+def render_collection_summary_page():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    return render_template('collection_summary.html')
