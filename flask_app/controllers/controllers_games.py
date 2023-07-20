4c75bcf626faef21b969f39e41fd65eb5c879238
Brandon Colangelo <brandoncancode@gmail.com>
2023-07-20 13:30:42 -0400

    Working on the render games controller.

--- flask_app/controllers/controllers_games.py
+++ flask_app/controllers/controllers_games.py
@@ -1,6 +1,8 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
 from flask_app.models import models_user, models_game
+from pprint import pprint
+import requests
 
 # GET Routes
 # Route for rendering the "consoles" page.
@@ -15,6 +17,15 @@ def render_consoles_page():
 def render_games_page():
     if 'user_id' not in session:
         return redirect('/logout')
+    # """ Get request to Video Games Database Api for all games"""
+    # api_key = "90fdbb86a3864e1ca9ba0dfdd948a58f"
+    # url = f"https://rawg-video-games-database.p.rapidapi.com/games?key={api_key}"
+    # headers = {
+    #     "X-RapidAPI-Key": "7b12899369msh6c9c430681eef3ep1f7973jsn1dcd54b8a95f",
+    #     "X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com"
+    # }
+    # response = requests.get(url, headers=headers)
+    # pprint(response.json())
     return render_template('games.html')
 
 # Route for rendering the "peripherals" page.
